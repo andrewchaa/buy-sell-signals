6e3ebdea3e0e79a714f4f08f8c8d518576915854
Andrew Chaa <achaa@kooth.com>
2023-04-13 15:44:04 +0100

    hourly alert

--- src/decide-buysell/hourly.py
+++ src/decide-buysell/hourly.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import yfinance as yf
 from sendgrid.helpers.mail import *
 from datetime import date
@@ -8,28 +9,29 @@ import mailer
 tickers = ["^IXIC", "GC=F", "^N225"]
 names = ["Nasdaq", "Gold", "Nikkei 225"]
 charts = []
-content = ''
-html_content = ''
 
 for i, ticker in enumerate(tickers):
+    content = ''
+    html_content = ''
     yfTicker = yf.Ticker(ticker)
     dailyDf = yfTicker.history(period="3mo", interval="1d")
     signals = ema.get_signals(dailyDf)
     latest_ema = signals.tail(1)['ema'].values[0]
 
-    content += f'EMA Signals for {names[i]}: \n'
-    content += f'{signals.tail()}\n\n'
-
-    houryDf = yfTicker.history(period="1d", interval="1h")
-    houryDf['Ema'] = latest_ema
+    hourlyDf = yfTicker.history(period="1d", interval="1h")
+    reviewDf = hourlyDf[['Close']].tail(3)
+    reviewDf['Ema'] = latest_ema
+    reviewDf['Signal'] = np.where(
+        reviewDf['Close'] > reviewDf['Ema'], 1.0, 0.0)
+    reviewDf['Signal_Position'] = reviewDf['Signal'].diff()
 
     content += f'Hourly data for {names[i]}: \n'
-    content += f'{houryDf.tail(5)}\n\n'
-
+    content += f'{reviewDf}\n\n'
     print(content)
-    html_content += f'<p>EMA Signals for {names[i]}:</p>\n'
-    html_content += f'{signals.tail().to_html()}<br /><br />\n\n'
 
-# to_emails = os.environ.get('SENDGRID_TO_EMAILS').split(',')
-# mailer.send(
-#     to_emails, f"Hourly EMA Signal alerts for {date.today()}", html_content, [])
+    html_content += f'<p>Hourly alert for {names[i]}:</p>\n'
+    html_content += f'{reviewDf.to_html()}<br /><br />\n\n'
+    if (reviewDf['Signal_Position'] == 1.0).any():
+        to_emails = os.environ.get('SENDGRID_TO_EMAILS').split(',')
+        mailer.send(
+            to_emails, f"Hourly Signal alerts on {date.today()} for {names[i]} ", html_content, [])
